1c5f70e627bf788a0b65b7329c1394d2b8a8670f
Esteban Zuluaga Gómez <69682830+Zuluaga1@users.noreply.github.com>
2020-09-14 22:31:22 -0500

    Add files via upload

---
+++ MyWebApp/script.js
@@ -0,0 +1,71 @@
+var map;
+var markers = [];
+const map_bounds = {north: 85, south: -85,west:-179.9999,east:180};
+
+// Inicialización de mapa
+async function iniciarMap() {
+  const _location = await getData();
+  const coord = { lat: _location[0], lng: _location[1] };
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 2,
+    center: coord,
+    minZoom: 2,
+    restriction: {latLngBounds:map_bounds}
+  });
+  marker = new google.maps.Marker({
+    position: coord,
+    map: map
+  });
+  markers[0] = marker;
+  await updateMarker();
+}
+// Función de obtención de datos
+async function getData() {
+  const _location = [];
+  // Obtener datos del listener remoto
+  const response = await fetch('/loc', {method: 'GET'});
+  const data = await response.json();
+  row = data.split(',');
+  _location.push(parseFloat(row[0]));
+  _location.push(parseFloat(row[1]));
+  _location.push(row[2]);
+  return _location;
+}
+
+// Actualizar marcador de localizacion
+async function updateMarker() {
+  try {
+    const _location = await getData();
+    const coord = { lat: _location[0], lng: _location[1] };
+    markers[0].setPosition(coord);
+    _changeText(_location);
+  } catch (error) {
+    console.log(error);
+  }
+  setTimeout(updateMarker, 1000);
+}
+// Función de actualización de datos
+function _changeText(_location) {
+  document.getElementById("lat_text").innerHTML = _location[0];
+  document.getElementById("long_text").innerHTML = _location[1];
+  const date = _location[2].split(" ");
+  document.getElementById("time_text").innerHTML = date[0] + " " + date[1];
+  document.getElementById("date_text").innerHTML = date[2];
+}
+// Función para centrar el mapa al marcador
+function centerMap(){
+  map.setCenter(markers[0].getPosition());
+}
+// Pasos extra para el panel de datos
+document.getElementById("CoordPanel").style.display = "none";
+document.getElementById("hidec").style.display = "none";
+document.getElementById("showc").addEventListener("click", function () {
+  document.getElementById("CoordPanel").style.display = "block";
+  document.getElementById("hidec").style.display = "block";
+  document.getElementById("showc").style.display = "none";
+})
+document.getElementById("hidec").addEventListener("click", function () {
+  document.getElementById("CoordPanel").style.display = "none";
+  document.getElementById("hidec").style.display = "none";
+  document.getElementById("showc").style.display = "block";
+})
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+const mysql= require('mysql');
+
+//create connection
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    clave: "admin",
+    database: "location"
+        
+});
+//connect
+/* db.connect((err) => {
+    if (err){
+        throw err;
+    
+    }    
+    console.log('Mysql Connected...');
+}); */
+// add data
+ 
+/*  db.connect(function(err) {
+     if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO data (latitud, longitud, time, date) VALUES ( _message,  1,  2, '5')";
+     db.query(sql, function (err, result) {
+       if (err) throw err;
+       console.log("1 record inserted");
+     });
+   });
+  */
+
+
+
+app.listen(10000);
+app.use(express.static('MyWebApp'));
+
+
+// UDP Listener
+const dgram = require('dgram');
+const socket = dgram.createSocket('udp4');
+
+
+socket.bind(10840);
+
+// Message receive
+socket.on('message', (msg, rinfo) => {
+    _message = msg.toString();
+    console.log(_message);
+});
+
+// Response handler
+app.get('/loc', function (req, res){
+    res.end(JSON.stringify(_message));
+});
